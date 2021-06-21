ae5308087e8d9fa4200cb27c2f4049a69af13639
Dan <denise97220@gmail.com>
2021-06-22 01:13:15 +0800

    working on new restaurant form

--- src/router/index.js
+++ src/router/index.js
@@ -7,61 +7,81 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'root',
-    redirect: '/restaurants'
+    path: "/",
+    name: "root",
+    redirect: "/restaurants",
   },
   {
-    path: '/signin',
-    name: 'sign-in',
-    component: SignIn
+    path: "/signin",
+    name: "sign-in",
+    component: SignIn,
   },
   {
-    path: '/signup',
-    name: 'sign-up',
-    component: () => import('../views/SignUp.vue')
+    path: "/signup",
+    name: "sign-up",
+    component: () => import("../views/SignUp.vue"),
   },
   {
-    path: '/restaurants',
-    name: 'restaurants',
-    component: Restaurants
+    path: "/restaurants",
+    name: "restaurants",
+    component: Restaurants,
   },
   {
-    path: '/restaurants/feed',
-    name: 'restaurants-feed',
-    component: () => import('../views/RestaurantsFeed.vue')
+    path: "/restaurants/feed",
+    name: "restaurants-feed",
+    component: () => import("../views/RestaurantsFeed.vue"),
   },
   {
-    path: '/restaurants/top',
-    name: 'restaurants-top',
-    component: () => import('../views/RestaurantsTop.vue')
+    path: "/restaurants/top",
+    name: "restaurants-top",
+    component: () => import("../views/RestaurantsTop.vue"),
   },
   {
-    path: '/restaurants/:id',
-    name: 'restaurant',
-    component: () => import('../views/Restaurant.vue')
+    path: "/restaurants/:id",
+    name: "restaurant",
+    component: () => import("../views/Restaurant.vue"),
   },
   {
-    path: '/restaurants/:id/dashboard',
-    name: 'restaurant-dashboard',
-    component: () => import('../views/RestaurantDashboard.vue')
+    path: "/restaurants/:id/dashboard",
+    name: "restaurant-dashboard",
+    component: () => import("../views/RestaurantDashboard.vue"),
   },
   {
-    path: '/users/top',
-    name: 'users-top',
-    component: () => import('../views/UsersTop.vue')
+    path: "/users/top",
+    name: "users-top",
+    component: () => import("../views/UsersTop.vue"),
   },
   {
-    path: '/user/:id',
-    name: 'user',
-    component: () => import('../views/User.vue')
+    path: "/user/:id",
+    name: "user",
+    component: () => import("../views/User.vue"),
   },
   {
-    path: '*',
-    name: 'not-found',
-    component: () => import('../views/NotFound.vue')
-  }
-]
+    path: "/admin",
+    exact: true,
+    redirect: "/admin/restaurants",
+  },
+  {
+    path: "/admin/restaurants",
+    name: "admin-restaurants",
+    component: () => import("../views/AdminRestaurants.vue"),
+  },
+  {
+    path: "/admin/restaurants/new",
+    name: "admin-restaurants-new",
+    component: () => import("../views/AdminRestaurantNew.vue"),
+  },
+  {
+    path: "/admin/restaurants/:id",
+    name: "admin-restaurant",
+    component: () => import("../views/AdminRestaurant.vue"),
+  },
+  {
+    path: "*",
+    name: "not-found",
+    component: () => import("../views/NotFound.vue"),
+  },
+];
 
 const router = new VueRouter({
   linkExactActiveClass: 'active',
--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -1,10 +1,19 @@
-import moment from 'moment'
+import moment from "moment";
 
 export const fromNowFilter = {
-    filters: {
-        fromNow(date) {
-            if(!date) return '-'
-            return moment(date).fromNow()
-        }
+  filters: {
+    fromNow(date) {
+      if (!date) return "-";
+      return moment(date).fromNow();
+    },
+  },
+};
+
+export const emptyImageFilter = {
+  filters: {
+    emptyImageFilter(src) {
+      // 如果圖片為空，回傳 No image 的圖片
+      return src || 'https://via.placeholder.com/350x220/DFDFDF?text=No+Image'
     }
-}
+  },
+};
